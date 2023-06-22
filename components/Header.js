0455cb90835b7d523dcdd4dd7583ece88c55bc5c
devkapsakrisz <devkapsakrisz@gmnail.com>
2023-06-22 21:19:55 +0100

    searchinput change

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,12 @@ import react from 'react';
 import Image from 'next/image';
 import {SearchIcon} from '@heroicons/react/solid'
 import {GlobeAltIcon,MenuIcon,UserCircleIcon,UsersIcon,} from "@heroicons/react/solid";
+import {useState} from 'react';
 
 function Header () {
 
 
-    const searchInput = '';
+    const [searchInput,setSearchInput] = useState('');
     return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10'>
         
@@ -20,7 +21,10 @@ function Header () {
         </div>
 
         <div className='flex items-center md:border-2 rounded-full py-2'>
-            <input className='flex-grow pl-5 text-gray-500 bg-transparent placeholder-gray-400 outline-none' type="text" placeholder="type your search" />
+            <input 
+            value={searchInput}
+            
+            className='flex-grow pl-5 text-gray-500 bg-transparent placeholder-gray-400 outline-none' type="text" placeholder="type your search" />
             <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 rounded-full text-white p-2 cursor-pointer'/>
         </div>
 
