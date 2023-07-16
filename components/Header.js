4fd29f0e057aad615f051d73b21698f873580177
devkapsakrisz <devkapsakrisz@gmnail.com>
2023-07-16 18:19:18 +0100

    working on search page

--- components/Header.js
+++ components/Header.js
@@ -10,12 +10,15 @@ import React ,{ useState } from "react";
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
+import { useRouter } from 'next/router';
 
 function Header() {
   const [searchInput, setSearchInput] = useState("");
   const [startDate,setStartDate] = useState(new Date());
   const [endDate,setEndDate] = useState(new Date());
   const [noOfGuests,setNoOfGuests]=useState(1);
+
+  const router = useRouter(); 
   const selectionRange ={
     startDate:startDate,
     endDate:endDate,
@@ -32,12 +35,23 @@ function Header() {
     setSearchInput("");
   };
 
+  const search = ()=> {
+    router.push({
+      pathname:"/search",
+      query: {
+        location:searchInput,
+        startDate:startDate.toISOString(),
+        endDate:endDate.toISOString(),
+        noOfGuests,
+      },
+    });
+  };
   
 
   
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10'>
-      <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+      <div onClick={()=>router.push("/")} className='relative flex items-center h-10 cursor-pointer my-auto'>
         <Image
           className='rounded-full'
           src="/kkbnbv2.jpg"
@@ -84,7 +98,7 @@ function Header() {
           </div>
           <div className='flex'>
             <button onClick={resetInput} className='flex-grow text-gray-400'>Cancel</button>
-            <button className='flex-grow text-red-300'>Search</button>
+            <button onClick={search} className='flex-grow text-red-300'>Search</button>
           </div>
         </div>
       )}
--- pages/search.js
+++ pages/search.js
@@ -1,19 +1,26 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { useRouter } from "next/router";
 import React from "react";
 
 
 function Search () {
+
+    const router=useRouter();
+    const {location,startDate,endDate,noOfGuests} =router.query;
+    
+
+
     return(
         <div>
                 <Header/>
                 <main className="flex">
                     <section className="flex-grow pt-14 px-6">
                         <p className="text-xs">
-                            300+ Stays for 5 number of guests
+                            300+ Stays for {noOfGuests} guests
                         </p>
 
-                        <h1 className="text-3xl font-semibold mt-2 mb-6 ">stays somewhere</h1>
+                        <h1 className="text-3xl font-semibold mt-2 mb-6 ">stays in {location}</h1>
 
                         <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-700 whitespace-nowrap">
                             <p className="button">Cancelation Flexibility</p>
